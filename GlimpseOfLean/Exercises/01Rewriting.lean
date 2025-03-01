29b80d633de78f32f3179bba47a9b4c84be184d9
Peng Wang <wangpeng@google.com>
2025-03-01 09:29:20 +0000

    Solved Rewriting and Iff.

--- GlimpseOfLean/Exercises/01Rewriting.lean
+++ GlimpseOfLean/Exercises/01Rewriting.lean
@@ -25,7 +25,7 @@ your proof is finished.
 -/
 
 example (a b : ℝ) : (a+b)^2 = a^2 + 2*a*b + b^2 := by {
-  sorry
+  ring
 }
 
 /- In the first example above, take a closer look at where Lean displays parentheses.
@@ -65,7 +65,7 @@ example (a b c d : ℝ) (h : a = b + c) (h' : b = d - e) : a + e = d + c := by {
 }
 
 /-
-Note that putting your cursor between `h` and`h'` shows you the intermediate proof state.
+Note that putting your cursor between `h` and `h'` shows you the intermediate proof state.
 
 Note also the subtle background color change in the tactic state that show you in green
 what is new and in red what is about to change.
@@ -75,7 +75,9 @@ but it doesn't use the assumptions `h` and `h'`
 -/
 
 example (a b c d : ℝ) (h : b = d + d) (h' : a = b + c) : a + b = c + 4 * d := by {
-  sorry
+  rw [h']
+  rw [h]
+  ring
 }
 
 /- ## Rewriting with a lemma
@@ -114,7 +116,10 @@ right-hand side.
 -/
 
 example (a b c : ℝ) : exp (a + b - c) = (exp a * exp b) / (exp c * exp 0) := by {
-  sorry
+  rw [exp_zero]
+  rw [exp_sub]
+  rw [exp_add]
+  ring
 }
 
 /-
@@ -138,7 +143,7 @@ by the left-hand side, so it will look for `b + c` in the current goal and repla
 -/
 
 example (a b c d : ℝ) (h : a = b + b) (h' : b = c) (h'' : a = d) : b + c = d := by {
-  sorry
+  rw [← h', ← h, h'']
 }
 
 /- ## Rewriting in a local assumption
@@ -180,11 +185,11 @@ Let's do some exercises using `calc`.
 
 example (a b c : ℝ) (h : a = b + c) : exp (2 * a) = (exp b) ^ 2 * (exp c) ^ 2 := by {
   calc
-    exp (2 * a) = exp (2 * (b + c))                 := by sorry
-              _ = exp ((b + b) + (c + c))           := by sorry
-              _ = exp (b + b) * exp (c + c)         := by sorry
-              _ = (exp b * exp b) * (exp c * exp c) := by sorry
-              _ = (exp b) ^ 2 * (exp c)^2           := by sorry
+    exp (2 * a) = exp (2 * (b + c))                 := by rw [h]
+              _ = exp ((b + b) + (c + c))           := by ring
+              _ = exp (b + b) * exp (c + c)         := by rw [exp_add]
+              _ = (exp b * exp b) * (exp c * exp c) := by rw [exp_add, exp_add]
+              _ = (exp b) ^ 2 * (exp c)^2           := by ring
 }
 
 /-
@@ -199,7 +204,10 @@ Aligning the equal signs and `:=` signs is not necessary but looks tidy.
 -/
 
 example (a b c d : ℝ) (h : c = d*a + b) (h' : b = a*d) : c = 2*a*d := by {
-  sorry
+  calc
+    c = d*a + b      := by exact h
+    _ = d*a + a*d    := by rw [h']
+    _ = 2*a*d        := by ring
 }
 
 /-
@@ -210,4 +218,3 @@ a Lean proof looks like and have learned about the following tactics:
 * `exact`
 * `calc`
 -/
-

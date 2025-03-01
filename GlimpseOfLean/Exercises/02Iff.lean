a324cfc43ec157e0281ef10799f371ba237d3307
Peng Wang <wangpeng@google.com>
2025-03-01 09:29:20 +0000

    Solved Rewriting and Iff.

--- GlimpseOfLean/Exercises/02Iff.lean
+++ GlimpseOfLean/Exercises/02Iff.lean
@@ -35,7 +35,11 @@ prove one-by-one.
 -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  apply add_pos
+  apply sq_pos_of_pos
+  exact ha
+  apply sq_pos_of_pos
+  exact hb
 }
 
 /-
@@ -60,7 +64,15 @@ example (a : ℝ) (ha : 0 < a) : 0 < (a^2)^2 := by {
 /- Now prove the same lemma as before using forwards reasoning. -/
 
 example (a b : ℝ) (ha : 0 < a) (hb : 0 < b) : 0 < a^2 + b^2 := by {
-  sorry
+  have h_a2: 0 < a^2 := by
+    apply sq_pos_of_pos
+    exact ha
+  have h_b2: 0 < b^2 := by
+    apply sq_pos_of_pos
+    exact hb
+  apply add_pos
+  exact h_a2
+  exact h_b2
 }
 
 
@@ -114,7 +126,11 @@ Let's prove a variation
 -/
 
 example {a b : ℝ} (c : ℝ) : a + c ≤ b + c ↔ a ≤ b := by {
-  sorry
+  rw [← sub_nonneg]
+  have h: b + c - (a + c) = b - a := by
+    ring
+  rw [h]
+  rw [sub_nonneg]
 }
 
 /-
@@ -152,7 +168,9 @@ example {a b : ℝ}  (ha : 0 ≤ a) : b ≤ a + b := by {
 /- Let's do a variant using `add_le_add_iff_left a : a + b ≤ a + c ↔ b ≤ c` instead. -/
 
 example (a b : ℝ) (hb : 0 ≤ b) : a ≤ a + b := by {
-  sorry
+  calc
+    a = a + 0 := by ring
+    _ <= a + b := by exact (add_le_add_iff_left a).2 hb
 }
 
 /-
@@ -183,7 +201,15 @@ example (a b : ℝ) : (a-b)*(a+b) = 0 ↔ a^2 = b^2 := by {
 /- You can try it yourself in this exercise. -/
 
 example (a b : ℝ) : a = b ↔ b - a = 0 := by {
-  sorry
+  constructor
+  . intro h
+    rw [h]
+    ring
+  . intro h
+    calc
+      a = a + 0 := by ring
+      _ = b := by rw [← h]
+                  ring
 }
 
 /-
